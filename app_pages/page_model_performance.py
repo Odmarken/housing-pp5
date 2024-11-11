e6f8bb437d04854ffd7015ea3bbef3b1bb191d7d
Elias Ödmark <Elias.odmarkk@gmail.com>
2024-11-11 23:01:33 +0000

    added pipline for errors

--- app_pages/page_model_performance.py
+++ app_pages/page_model_performance.py
@@ -3,8 +3,11 @@ import pandas as pd
 from src.data_management import load_pkl_file
 from src.machine_learning.model_evaluation import regression_performance
 
+
 def app():
     st.write("### Model Performance")
+
+    # Load pipeline and data
     pipeline = load_pkl_file("outputs/ml_pipeline/predict_SalePrice/v1/pipeline.pkl")
     X_train = pd.read_csv("outputs/ml_pipeline/predict_SalePrice/v1/X_train.csv")
     y_train = pd.read_csv("outputs/ml_pipeline/predict_SalePrice/v1/y_train.csv")
